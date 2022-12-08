18ae3fc6a152e566f9b395c0e5598c8ffacffbfc
jozebel11 <jjhardwicke@hotmail.co.uk>
2022-12-08 22:43:57 +0000

    Add trainer-profile and make changes to header styling

--- src/Header.js
+++ src/Header.js
@@ -4,18 +4,23 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
-import Container from 'react-bootstrap/Container'
+import Container from 'react-bootstrap/Container';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import Accordion from 'react-bootstrap/Accordion';
+
 
 
 const Header = () => {
    
   return (
+    <div className='header'>
     <>
       {[false].map((expand) => (
         <Navbar key={expand} bg="light" expand={expand} className="mb-3">
-          <Container fluid>
-            <Navbar.Brand href="/">Strong Brew Social</Navbar.Brand>
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+          <Container className='header-container' fluid>
+            <div></div>
+            <Navbar.Brand className='header-brand' href="/">Strong Brew Social</Navbar.Brand>
+            <Navbar.Toggle className='header-toggle' aria-controls={`offcanvasNavbar-expand-${expand}`} />
             <Navbar.Offcanvas
               id={`offcanvasNavbar-expand-${expand}`}
               aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
@@ -28,8 +33,42 @@ const Header = () => {
               </Offcanvas.Header>
               <Offcanvas.Body>
                 <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link href="#action1">Home</Nav.Link>
-                  <Nav.Link href="#action2">Link</Nav.Link>
+                    <Accordion>
+                    <Accordion.Item eventKey="0">
+                    <Accordion.Header>
+                     <div className='trainer-profile'>
+                       <span className='trainer-name'>Elliott John</span>
+                       <Nav.Link href="https://www.instagram.com/thesciencediet/"><InstagramIcon/></Nav.Link>
+                     </div>
+                     </Accordion.Header>
+                     <Accordion.Body>
+                       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                       eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                       minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                       aliquip ex ea commodo consequat. Duis aute irure dolor in
+                       reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                       pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                       culpa qui officia deserunt mollit anim id est laborum.
+                     </Accordion.Body>
+                     </Accordion.Item>
+                     <Accordion.Item eventKey="1">
+                    <Accordion.Header>
+                     <div className='trainer-profile'>
+                       <span className='trainer-name'>Trainer</span>
+                       <Nav.Link href="https://www.instagram.com/thesciencediet/"><InstagramIcon/></Nav.Link>
+                     </div>
+                     </Accordion.Header>
+                     <Accordion.Body>
+                       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                       eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                       minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                       aliquip ex ea commodo consequat. Duis aute irure dolor in
+                       reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                       pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                       culpa qui officia deserunt mollit anim id est laborum.
+                     </Accordion.Body>
+                     </Accordion.Item>
+                  </Accordion>
                 </Nav>
               </Offcanvas.Body>
             </Navbar.Offcanvas>
@@ -37,6 +76,7 @@ const Header = () => {
         </Navbar>
       ))}
     </>
+    </div>
   )
 }
 
